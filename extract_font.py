5522444f5defbfcca54d0d74a2a7ad8d290c75f4
SkyEye_FAST <skyeyefast@teahou.se>
2024-01-20 02:14:30 +0800

    Test workflow

---
+++ extract_font.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+"""字体子集生成器"""
+
+import json
+from pathlib import Path
+from fontTools import subset
+
+P = Path(__file__).resolve().parent
+
+# 读取语言文件
+with open(P / "lang" / "lzh.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# 提取字符串中包含的所有字符
+all_char = {char for value in data.values() for char in str(value)}
+all_char.discard("\n")  # 去除换行符
+
+args = [
+    "I.Ming-8.00.ttf",
+    f"--text={"".join(all_char)}",
+    f"--output-file={str(P / Path(r"static\fonts\I.Ming-8.00.subset.woff2"))}",
+    "--flavor=woff2",
+]
+
+subset.main(args)
