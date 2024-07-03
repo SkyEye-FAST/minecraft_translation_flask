f985c7c0c425dd5d6d9aec14dbfac29368670156
SkyEye_FAST <skyeyefast@teahou.se>
2024-07-03 23:58:07 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fieldsets.forEach(fieldset => {
         const button = fieldset.querySelector(".toggle-button");
         const fieldsetId = fieldset.id;
-        const isCollapsed = localStorage.getItem(fieldsetId) === "true";
+        const isCollapsed = localStorage.getItem(fieldsetId) === "false";
 
         if (isCollapsed) {
             fieldset.classList.add("collapsed");
