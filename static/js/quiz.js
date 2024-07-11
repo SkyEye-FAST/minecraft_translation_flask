9a94b2453f444aaf734a585eccfa267292fbb5a0
SkyEye_FAST <skyeyefast@teahou.se>
2024-07-11 23:33:21 +0800

    Reproduce in Chromium

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -6,28 +6,29 @@ $(document).ready(function () {
     const fadeDuration = 300;
 
     function createBoxes(length) {
-        const boxesDiv = $('#boxes').empty();
+        const boxesDiv = $("#boxes").empty();
         for (let i = 0; i < length; i++) {
-            $('<div>', {
-                class: 'box',
-                id: 'box' + (i + 1)
+            $("<div>", {
+                class: "box",
+                id: "box" + (i + 1),
             }).appendTo(boxesDiv);
         }
     }
 
     function loadQuestion() {
-        const inputBox = $('#inputBox').val('');
+        $("#inputBox").val("");
+
         const currentKey = questionKeys[currentQuestionIndex];
         const question = questions[currentKey];
         const sourceText = question.source;
         const translationText = question.translation;
 
-        $('#info').fadeOut(fadeDuration, function () {
-            $('#sourceText').text(sourceText);
-            $('#keyText').text(currentKey);
+        $("#info").fadeOut(fadeDuration, function () {
+            $("#sourceText").text(sourceText);
+            $("#keyText").text(currentKey);
 
             const questionLength = translationText.length;
-            inputBox.attr('maxlength', questionLength);
+            $("#inputBox").attr("maxlength", questionLength);
             createBoxes(questionLength);
             updateBoxes();
 
@@ -36,39 +37,59 @@ $(document).ready(function () {
     }
 
     function updateBoxes() {
-        const input = $('#inputBox').val();
+        const input = $("#inputBox").val();
         const currentKey = questionKeys[currentQuestionIndex];
-        const question = questions[currentKey];
-
-        if (!question) {
-            console.error('Question not found for key: ', currentKey);
-            if (currentQuestionIndex < questionKeys.length) {
-                return;
-            } else {
-                showSummary();
-            }
-        }
-
-        const correctAnswer = question.translation;
+        const correctAnswer = questions[currentKey].translation;
 
         for (let i = 0; i < correctAnswer.length; i++) {
-            const box = $('#box' + (i + 1));
-            box.text(input[i] || '');
+            const box = $("#box" + (i + 1));
+            box.text(input[i] || "");
             if (input[i] === undefined) {
-                box.css('background-color', '#9ca3af25');
+                box.css("background-color", "#9ca3af25");
             } else if (input[i] === correctAnswer[i]) {
-                box.css('background-color', '#79b851');
+                box.css("background-color", "#79b851");
             } else if (correctAnswer.includes(input[i])) {
-                box.css('background-color', '#f3c237');
+                box.css("background-color", "#f3c237");
             } else {
-                box.css('background-color', '#9ca3af25');
+                box.css("background-color", "#9ca3af25");
             }
         }
+    }
+
+    function showSummary() {
+        $("#info, #inputBox").fadeOut(fadeDuration, function () {
+            $(this).hide();
+            const summaryTableBody = $("#summaryBody").empty();
+
+            questionKeys.forEach((key) => {
+                const question = questions[key];
+                const sourceText = question.source;
+                const translationText = question.translation;
+
+                const row = $("<tr>").appendTo(summaryTableBody);
+                $("<td>").text(sourceText).appendTo(row);
+                $("<td>").text(translationText).appendTo(row);
+            });
+
+            $("#summary").fadeIn(fadeDuration);
+        });
+    }
+
+    $("#restartButton").click(function () {
+        window.location.href = `../quiz/${randomCode}`;
+    });
+
+    $("#inputBox").on("input", updateBoxes);
+
+    $("#inputBox").on("keyup", function () {
+        const input = $(this).val();
+        const currentKey = questionKeys[currentQuestionIndex];
+        const correctAnswer = questions[currentKey].translation;
 
         if (input === correctAnswer) {
             setTimeout(() => {
                 currentQuestionIndex++;
-                console.log("当前问题索引：", currentQuestionIndex);
+                console.log("当前题目索引：", currentQuestionIndex);
                 console.log("当前键名：", currentKey);
                 Sentry.captureMessage(`Quiz, ${currentQuestionIndex}`);
                 if (currentQuestionIndex < questionKeys.length) {
@@ -78,54 +99,34 @@ $(document).ready(function () {
                 }
             }, delayBetweenQuestions);
         }
-    }
-
-    function showSummary() {
-        $('#info, #inputBox').fadeOut(fadeDuration, function () {
-            $(this).hide();
-            const summaryTableBody = $('#summaryBody').empty();
-
-            questionKeys.forEach((key) => {
-                const question = questions[key];
-                const sourceText = question.source;
-                const translationText = question.translation;
-
-                const row = $('<tr>').appendTo(summaryTableBody);
-                $('<td>').text(sourceText).appendTo(row);
-                $('<td>').text(translationText).appendTo(row);
-            });
-
-            $('#summary').fadeIn(fadeDuration);
-        });
-    }
-
-    $('#restartButton').click(function () {
-        window.location.href = `../quiz/${randomCode}`;
     });
 
-    $('#inputBox').on('input', updateBoxes);
-
+    // 加载首个题目
     loadQuestion();
 });
+
 
 $(document).ready(function () {
     var currentUrl = window.location.href;
     var match = currentUrl.match(/\/([^\/?#]+)[\/?#]?$/);
-    var lastSegment = match ? match[1] : '';
-    document.getElementById('last-segment').textContent = lastSegment;
+    var lastSegment = match ? match[1] : "";
+    document.getElementById("last-segment").textContent = lastSegment;
 
-    $('#copy-button').click(function () {
+    $("#copy-button").click(function () {
         var $copyButton = $(this);
-        var $lastSegment = $('#last-segment');
+        var $lastSegment = $("#last-segment");
         var lastSegmentContent = $lastSegment.text();
 
-        navigator.clipboard.writeText(lastSegmentContent).then(function () {
-            $copyButton.text('check');
-            setTimeout(function () {
-                $copyButton.text('content_copy');
-            }, 1500);
-        }).catch(function (err) {
-            console.error('Failed to copy: ', err);
-        });
+        navigator.clipboard
+            .writeText(lastSegmentContent)
+            .then(function () {
+                $copyButton.text("check");
+                setTimeout(function () {
+                    $copyButton.text("content_copy");
+                }, 1500);
+            })
+            .catch(function (err) {
+                console.error("Failed to copy: ", err);
+            });
     });
 });
