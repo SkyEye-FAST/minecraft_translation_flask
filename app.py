b8c345419a23963af1b1c0aa5652e7d6e879d65d
SkyEye_FAST <skyeyefast@teahou.se>
2024-07-14 02:46:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_locale() -> str:
         str: 返回匹配的语言代码，默认返回 "en"。
     """
 
-    return request.accept_languages.best_match(["zh", "en"], default="en")
+    return request.accept_languages.best_match(["zh-CN", "zh-TW", "zh", "en"], default="en")
 
 
 def get_timezone_from_ip() -> Optional[str]:
